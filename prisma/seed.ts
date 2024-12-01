0bc2d30a17871d056a0892fd4057475fc3a2bbc8
Mohit Soni <mohitsoni9731@gmail.com>
2024-12-01 14:42:17 +0530

    trying #2 to deploy Db on neon and app on vercel This time I have added seed command with build command in package.json and Added data in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { ClassList, Group, PrismaClient } from "@prisma/client";
+import { ClassList, Gender, Group, Month, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const run = async () => {
@@ -6,40 +6,40 @@ const run = async () => {
     data: [
       {
         id: ClassList.class_12,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_11,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_10,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_9,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_8,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_7,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
 
       {
         id: ClassList.class_6,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_5,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
       {
         id: ClassList.class_4,
-        group: Group.Sunflower
+        group: Group.Sunflower,
       },
     ],
   });
@@ -82,6 +82,110 @@ const run = async () => {
         classId: ClassList.class_12,
       },
     ],
+  });
+  await prisma.subject.createMany({
+    data: [
+      {
+        subjectName: "Maths",
+        classname: ClassList.class_12,
+      },
+      {
+        subjectName: "English",
+        classname: ClassList.class_11,
+      },
+    ],
+  });
+  await prisma.student.createMany({
+    data: [
+      {
+        username: "first",
+        firstname: "Mohit",
+        lastname: "Robbin",
+        motherName: "Sulekha",
+        fatherName: "Sunil",
+        gender: Gender.Male,
+        contactNo: "1234567894",
+        address: "bac newar color",
+        feesPaidUpto: Month.May,
+        classId: ClassList.class_12,
+        admissionYear: 2024,
+        dob: "21/02/2004",
+      },
+      {
+        username: "second",
+        firstname: "Love",
+        lastname: "Shekh",
+        motherName: "Hakib",
+        fatherName: "Shekh Salman",
+        gender: Gender.Male,
+        contactNo: "1234567894",
+        address: "bac newar color",
+        feesPaidUpto: Month.May,
+        classId: ClassList.class_12,
+        admissionYear: 2024,
+        dob: "21/02/2004",
+      },
+    ],
+  });
+  await prisma.expense.createMany({
+    data: [
+      {
+        monthName: Month.January,
+        date: 24,
+        amount: 12500,
+        year: 2024,
+        description: "School maintance check",
+        title: "School Inspection",
+      },
+      {
+        monthName: Month.March,
+        date: 1,
+        amount: 8250,
+        year: 2024,
+        description: "Anuual Function Expense for all classes",
+        title: "Annual Function 2024",
+      },
+    ],
+  });
+  await prisma.gain.createMany({
+    data: [
+      {
+        monthName: Month.January,
+        date: 28,
+        amount: 85623,
+        year: 2024,
+        description: "Total Rent for 5 Properties",
+        title: "Property Rent",
+      },
+      {
+        monthName: Month.March,
+        date: 1,
+        amount: 1250,
+        year: 2024,
+        description: "Maintance Pay for rent houses",
+        title: "Maintance Recieved",
+      },
+    ],
+  });
+  await prisma.teacher.create({
+    data: {
+      username: "first-Teacher",
+      firstname: "Suman",
+      lastname: "Lavesh",
+      qualification: "B.tech",
+      joiningDate: 5,
+      joiningYear: 2021,
+      joiningMonth: Month.February,
+      gender: Gender.Female,
+      lastSalaryPaid: Month.August,
+      contactNo: "1234567895",
+      address: "NEar student home",
+      salary: 45290,
+      classes: {
+        connect: [{ id: ClassList.class_12 }, { id: ClassList.class_11 }],
+      },
+      subjects:{}
+    },
   });
 };
 run().then(() => {
--- src/components/Admin/OutstandingDues/OutStandingAmount.ts
+++ src/components/Admin/OutstandingDues/OutStandingAmount.ts
@@ -1,42 +1,43 @@
 import prisma from "@/utils/db";
 
-import { months  } from "@/components/Extra";
+import { months } from "@/components/Extra";
 const getMonth = new Date().getMonth();
 
 const classId: string[] = [];
 const totalStudent: number[] = [];
 
-
 export const outstandingData = async (): Promise<{
   classId: string[];
   totalStudent: number[];
 }> => {
-  const result = await prisma.class
-    .findMany({
-      select: {
-        id: true,
-        totalStudents: {
-          select: {
-            feesPaidUpto: true,
-          },
+  const result = await prisma.class.findMany({
+    select: {
+      id: true,
+      totalStudents: {
+        select: {
+          feesPaidUpto: true,
         },
       },
-    })
-    .then((res) => {
-      return res.map((value) => {
-        const className = value.id.replace("class_", "").concat("th");
-        const count = value.totalStudents.filter((element) => {
-          const month = months.indexOf(element.feesPaidUpto);
-          return month + 1 < getMonth;
-        }).length;
-        classId.push(className);
-        totalStudent.push(count);
-        return {
-          classId,
-          totalStudent,
-        };
-      });
-    });
-
-  return result[0]
+    },
+  });
+  if (!result) {
+    return {
+      classId: [],
+      totalStudent: [],
+    };
+  }
+  const res = result.map((value) => {
+    const className = value.id.replace("class_", "").concat("th");
+    const count = value.totalStudents.filter((element) => {
+      const month = months.indexOf(element.feesPaidUpto);
+      return month + 1 < getMonth;
+    }).length;
+    classId.push(className);
+    totalStudent.push(count);
+    return {
+      classId,
+      totalStudent,
+    };
+  });
+  return res[0];
 };
